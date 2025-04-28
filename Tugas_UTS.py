eb876f0fbeeebdcaadb08f26e2ed3f57d019fba2
LahMerah <mpis.casttelo@gmail.com>
2025-04-28 21:48:12 +0700

    punya apip

---
+++ Tugas_UTS.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, make_response
+
+app = Flask(__name__)
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
